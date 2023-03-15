4979e9bc4e0ebe100805e406178708cdfc7af09a
xams-creator <1228552328@qq.com>
2023-03-15 14:22:15 +0800

    upgrade: 0.0.3 移除 hmr 内容到 dusk-hmr

--- src/index.ts
+++ src/index.ts
@@ -1,55 +1,8 @@
 import { PluginOption } from 'vite';
+import createViteDuskHmr from '@xams-framework/vite-plugin-dusk-hmr';
 
-const REG_MODEL = /\.model.(tsx|ts|js|jsx)/;
-const REG_MODEL_END = /\.model.(tsx|ts|js|jsx)$/;
-const REG_MODEL_END_QM = /\.model.(tsx|ts|js|jsx)\?$/;
-
-/*
- *   1.当正常导入 model 时, query为不带 v= 的字符串
- *   2.当使用dusk-plugin-vite 时，query将带 v=
- *
- * **/
-export function createViteDuskHMR(): PluginOption {
-    return {
-        name: 'vite-plugin-dusk-hmr',
-        transform(code, id) {
-            if (REG_MODEL.test(id)) {
-                return {
-                    code: `
-${code}
-
-if (import.meta.hot) {
-    const params = new URL(import.meta.url).searchParams;
-    if (!params.has('v')) {
-        import.meta.hot.accept((module) => {
-            const app = window.__DUSK_PLUGIN_VITE_HMR_APP_RUNTIME__;
-            if (app && module) {
-                app._mm.remove(module.default.namespace);
-                app.define(module.default);
-            }
-        });
-    }
-}
-`,
-                };
-            }
-        },
-        // handleHotUpdate({ server, modules }) {
-        // const ids = [];
-        // modules.forEach((module) => {
-        //     if (/\.model.(tsx|ts|js|jsx)$/.test(module.id)) {
-        //         ids.push(module.id);
-        //         // @ts-ignore
-        //         console.log(require(module.id).namespace)
-        //     }
-        // });
-        // console.log('en')
-        // server.ws.send({
-        //     type: 'custom',
-        //     event: 'dusk-model-update',
-        //     data: { },
-        // });
-        // return [];
-        // },
-    };
+export default function createViteDusk(): PluginOption {
+    return [
+        createViteDuskHmr(),
+    ];
 }
